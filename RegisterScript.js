28f92f5e5e467fef62d1d385d481d7b225f0afaf
Yiyi Tseng <vickey8910@gmail.com>
2023-05-30 15:19:42 +0800

    註冊頁面

---
+++ RegisterScript.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+   
+    // Button click volunteer event handler
+    $('#volunteer').on('click', function() {
+        $('#fullscreen-image').css('background-image', 'url("Resources/background.png")') // Change background 
+        $('#register').addClass('hidden');
+        $('#identity').addClass('hidden');
+        // Show message box
+        $('#volunteerMsgBox').removeClass('hidden');
+        $('#elderMsgBox').addClass('hidden');
+        $('body').append('<div class="translucent-overlay"></div>');
+        // Hide the button
+        $(this).hide();
+        $('#elder').hide();
+    });
+    // Button click elder event handler
+    $('#elder').on('click', function() {
+        $('#fullscreen-image').css('background-image', 'url("Resources/background.png")') // Change background 
+        $('#register').addClass('hidden');
+        $('#identity').addClass('hidden');
+        // Show message box
+        $('#elderMsgBox').removeClass('hidden'); 
+        $('#volunteerMsgBox').addClass('hidden'); 
+        $('body').append('<div class="translucent-overlay"></div>');
+        // Hide the button
+        $(this).hide();
+        $('#volunteer').hide();
+    });
+
+    // Close message box and remove the overlay
+    $('.closeMessageBox').on('click', function() {
+      $('#volunteerMsgBox').addClass('hidden');
+      $('#elderMsgBox').addClass('hidden');
+      $('.translucent-overlay').remove();
+    });
+
+});
+  
