2c51ef7546311ca6fb20643ab02619fed4ae4253
Yiyi Tseng <vickey8910@gmail.com>
2023-05-31 15:39:20 +0800

    主頁及註冊頁面

---
+++ MainPage.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    
+    $('#renewPassword').on('click', function() {
+        $('body').append('<div class="translucent-overlay"></div>');
+        $('#renewPasswordMsgBox').removeClass('hidden');
+    });
+
+
+});
